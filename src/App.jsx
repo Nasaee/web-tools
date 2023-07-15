ebc65fd143afe250591bd07e1908f7f46f9e7437
Nasaee <nasaee.dev@gmail.com>
2023-07-15 14:53:01 +0700

    set scroll-padding-top to index.css and App.jsx useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import Navbar from './containers/Navbar/Navbar';
 import Section from './containers/Section/Section';
 import { AiOutlineArrowUp } from 'react-icons/ai';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const navgationHeight = document.querySelector('.navbar').offsetHeight;
+    document.documentElement.style.setProperty(
+      '--scroll-padding',
+      navgationHeight - 1 + 'px'
+    );
+  }, []);
+
   return (
     <>
       <Navbar />
